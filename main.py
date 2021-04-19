52c7ab5a500c02717bb92cdae8fefa421f4bbc29
reversee <46657288+reversee-dev@users.noreply.github.com>
2021-04-19 22:39:33 +0200

    Nanofi the Code

--- main.py
+++ main.py
@@ -1,38 +1,21 @@
-import discord
-import os
-import random
-import asyncio
-import cffi
-import ffmpeg
-
+import discord, os, asyncio, cffi, ffmpeg
 client = discord.Client()
-
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Undertale OST | $undertale"))
-
+    print('Logged in using: {0.user}'.format(client))
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
+    if message.author == client.user: return
     if message.content.startswith('$play'):
         voice = discord.utils.get(client.voice_clients, guild=message.guild)
-
         if voice == None:
             channel = message.author.voice.channel
             voice = await channel.connect() 
-            
         files=os.listdir(".\\music")
         d=random.choice(files)
-
         voice.stop()
         voice.play(discord.FFmpegPCMAudio(source=".\music\\"+d))
-
-        embed=discord.Embed(title="Sans Muzykowanie", color=0x000000)
+        embed=discord.Embed(title="Sans Muzykowanie nano | https://github.com/reversee-dev/sansmuzykowanie | "color=0x000000)
         embed.add_field(name="Now playing:", value=d, inline=True)
-        embed.set_footer(text="by reversee | Sans Muzykowanie |")
         msgx = await message.channel.send(embed=embed)
-
 client.run("ODE5NTYyMjYxNjcxNTc1NjUz.YEoa1Q.x9WZOazzgSJ5Y-fxZE83s7jlipc")
