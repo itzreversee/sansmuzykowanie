c43b3bd859fb7c203cf68776229ceeb4b517186e
reversee <46657288+reversee-dev@users.noreply.github.com>
2021-11-01 18:41:25 +0100

    update 2.0
    rewrite, song selector and loop!

--- main.py
+++ main.py
@@ -1,38 +1,125 @@
 import discord
+from discord.ext import commands
+
 import os
 import random
 import asyncio
 import cffi
 import ffmpeg
+import time
 
-client = discord.Client()
+from mutagen.mp3 import MP3
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="$play | Play random undertale Music!"))
+# super quality code
+llock = "n"
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
-    if message.content.startswith('$play'):
-        voice = discord.utils.get(client.voice_clients, guild=message.guild)
+@bot.command()
+async def setup(ctx):
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="$help | Play random music!"))
 
-        if voice == None:
-            channel = message.author.voice.channel
-            voice = await channel.connect() 
-            
-        files=os.listdir(".\\music")
-        d=random.choice(files)
 
+@bot.command()
+async def play(ctx, song: str = None):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+    global llock
+    llock = "n"
+
+    if voice == None:
+        channel = ctx.author.voice.channel
+        voice = await channel.connect() 
+    
+    path = '/home/server/servers/sansmuzykowanie/music/'
+    if song == None:
+        song = random.choice(os.listdir(path)) 
+
+    else:
+        if os.path.isfile(path+song) == False:
+            song = random.choice(os.listdir(path)) 
+        
+    voice.stop()
+    voice.play(discord.FFmpegPCMAudio(source=path+song))
+
+    embed=discord.Embed(title="Sans Muzykowanie", color=0x000000)
+    embed.add_field(name="Now playing:", value=song, inline=True)
+    embed.set_footer(text="by reversee#2134 | Sans Muzykowanie |")
+    msgx = await ctx.channel.send(embed=embed)
+
+@bot.command()
+async def stop(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice.stop()
+    
+    global llock
+    llock = "n"
+
+@bot.command()
+async def list(ctx):
+    path = '/home/server/servers/sansmuzykowanie/music/'
+    d = os.listdir(path)
+    n = 0
+    
+    embed=discord.Embed(title="Sans Muzykowanie", color=0x000000)
+    embed=discord.Embed(description="List of songs ( use $play \"song\" to play exact song)")
+
+    for i in d:
+        n = n + 1
+        embed.add_field(name=n, value=i, inline=True)
+
+
+    embed.set_footer(text="by reversee#2134 | Sans Muzykowanie |")
+    msgx = await ctx.channel.send(embed=embed)
+
+@bot.command()
+async def loop(ctx, song: str = None):
+
+    rSong = False
+
+    path = '/home/server/servers/sansmuzykowanie/music/'
+    if song == None:
+        song = random.choice(os.listdir(path)) 
+        rSong = True
+    else:
+        if os.path.isfile(path+song) == False:
+            song = random.choice(os.listdir(path)) 
+            rSong = True
+
+    tsong = song
+    if rSong == True:   
+        tsong = "Random!" 
+    
+    embed=discord.Embed(title="Sans Muzykowanie", color=0x000000)
+    embed.add_field(name="Now playing:", value=tsong, inline=True)
+    embed.add_field(name="Loop enabled!", value="Use $play to disable.", inline=True)
+    embed.set_footer(text="by reversee#2134 | Sans Muzykowanie |")
+    msgx = await ctx.channel.send(embed=embed)
+        
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+    if voice == None:
+        channel = ctx.author.voice.channel
+        voice = await channel.connect() 
+    
+    #copied from play
+    global llock 
+    llock = "y"
+    while llock == "y":  
         voice.stop()
-        voice.play(discord.FFmpegPCMAudio(source=".\music\\"+d))
+        voice.play(discord.FFmpegPCMAudio(source=path+song))
 
-        embed=discord.Embed(title="Sans Muzykowanie", color=0x000000)
-        embed.add_field(name="Now playing:", value=d, inline=True)
-        embed.set_footer(text="by reversee-dev <-- Github | Sans Muzykowanie |")
-        msgx = await message.channel.send(embed=embed)
+        sSong = MP3(path + song)
 
-client.run("TOKEN")
+        await asyncio.sleep(sSong.info.length)
+
+        if rSong == True:
+            song = random.choice(os.listdir(path))
+            msgx = await ctx.channel.send("changing to: (random) - " + song)
+        
+        if llock == "n":
+            break
+
+    #end of copy
+    
+bot.run("ur_token_here")
